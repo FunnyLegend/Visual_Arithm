b830a3f5115faf25ae246c8084f1de4bd3ccaacb
FunnyLegend <58954723+FunnyLegend@users.noreply.github.com>
2020-05-28 09:02:17 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import  Arithm.ArithmOP;
+public class Main {
+    public static void main(String args[]) {
+        ArithmOP p=new ArithmOP();
+int a=2;
+int b =3;
+
+System.out.println(p.add(a,b));
+System.out.println(p.mul(a,b));
+
+    }
+}
